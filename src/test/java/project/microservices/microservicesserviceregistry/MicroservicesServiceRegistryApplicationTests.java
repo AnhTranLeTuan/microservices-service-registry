7f2821f45992be8150d73a6fc28e9ea0e2875f2f
Anh Tran Le Tuan <anh.letuan.tran.computer@gmail.com>
2024-01-05 07:59:31 -0600

    first commit

---
+++ src/test/java/project/microservices/microservicesserviceregistry/MicroservicesServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package project.microservices.microservicesserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
